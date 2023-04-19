00fe1b0f14f9fdd35873b062036577eb4f8760b4
RHILOF2 <105368274+RHILOF2@users.noreply.github.com>
2023-04-19 15:46:42 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { MantineProvider, Text } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <MantineProvider withGlobalStyles withNormalizeCSS>
     <App />
-  </React.StrictMode>,
+  </MantineProvider>,
 )
