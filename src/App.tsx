2e43982e70c03331016941db57e6955b81d3e1b9
RHILOF2 <105368274+RHILOF2@users.noreply.github.com>
2023-04-19 15:46:27 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { MantineProvider, Text } from '@mantine/core';
-import  TodoList  from './components/TodoList/TodoList';
+import TodoList from './components/TodoList/TodoList';
+import Info from './components/Info/Info';
+import Todo from './components/TodoList/Todo';
+import { Card, Center } from '@mantine/core';
 
-function App() {
+export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <App />
-      <TodoList />
-    </MantineProvider>
+    <>
+    <Center>
+      <Card shadow="sm" padding="lg" radius="md" maw={800} mx="auto" withBorder>
+        <TodoList />
+        <Todo />
+        <Info />
+      </Card>
+    </Center>
+      
+    </>
   );
 }
-export default App;
